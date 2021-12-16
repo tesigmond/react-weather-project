5554f72d4633868f8f788e8b88625e8756458018
tesigmond <tesigmond@gmail.com>
2021-12-16 13:59:26 -0800

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather app
-      <p>
-        This website is coded by Tory Sigmond and is{" "}
-        <a
-          href="https://github.com/tesigmond/react-weather-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-      </p>
+      <div className="container">
+        Weather app
+        <p>
+          This website is coded by Tory Sigmond and is{" "}
+          <a
+            href="https://github.com/tesigmond/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
