c251a17e36f1fd4600fdb49d56bb3cbeab2bd4f9
tesigmond <tesigmond@gmail.com>
2022-01-08 12:12:31 -0800

    Trying to add forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,32 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 import axios from "axios";
 
 export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
-    console.log(response.data);
+    console.log(response);
+    console.log(response.data.daily);
+    setForecast(response.data.daily);
+    setLoaded(true);
+    console.log(forecast);
+    console.log(loaded);
   }
 
-  let apiKey = "eaf7d01f74cab187ef4b58c7a2bb6662";
-  let longitude = props.coordinates.lon;
-  let latitude = props.coordinates.lat;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
+  function load() {
+    let apiKey = "96419952d8c185a058265bff1346020d";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
 
-  axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="WeatherForecast-day">Thu</div>
-          <WeatherIcon code="13n" size={36} />
-          <div className="WeatherForecast-temperatures">
-            <span className="WeatherForecast-temperatures-max">44</span>
-            <span className="WeatherForecast-temperatures-min">42</span>
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <div className="WeatherForecast-day">Thu</div>
+            <WeatherIcon code="13n" size={36} />
+            <div className="WeatherForecast-temperatures">
+              <span className="WeatherForecast-temperatures-max">50</span>
+              <span className="WeatherForecast-temperatures-min">42</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    load();
+    return null;
+  }
 }
